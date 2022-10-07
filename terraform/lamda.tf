a84f221cd91c38b50879fe62ca310f194b3c996a
John Carter <john_p_carter@yahoo.co.uk>
2022-10-07 23:58:21 +0100

    Fix: change lamda handler

--- terraform/lamda.tf
+++ terraform/lamda.tf
@@ -1,15 +1,22 @@
 module "lambda_function" {
   source = "terraform-aws-modules/lambda/aws"
 
-  function_name = "image-process"
+  function_name = "${var.project_name}-lambda"
   description   = "strip exif data from images"
-  handler       = "index.lambda_handler"
-  runtime       = "python3.9"
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.8"
 
   create_package         = false
-  local_existing_package = "../lambda_function/archive.zip"
+    local_existing_package = "../lambda_function/archive.zip"
+
+#    source_path = "../lambda_function"
 
   tags = {
-    Name = "image-processor-lambda"
+    Name = "${var.project_name}-lambda"
   }
+
+  attach_policies = true
+  policies           = ["arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole","arn:aws:iam::aws:policy/AmazonS3FullAccess"]
+  number_of_policies = 2
+  
 }
