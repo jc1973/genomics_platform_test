daec6daef06d6afaf3da1ebde583daf10683afdc
John Carter <john_p_carter@yahoo.co.uk>
2022-10-08 23:35:42 +0100

    Feature: create 2 users
    Read and Write UserA to input bucket
    Read Only UserB to processed bucket

---
+++ terraform/iam.tf
@@ -0,0 +1,98 @@
+resource "aws_iam_user" "UserA" {
+  name          = "UserA"
+  path          = "/"
+}
+
+resource "aws_iam_user" "UserB" {
+  name          = "UserB"
+  path          = "/"
+}
+
+
+resource "aws_iam_user_policy_attachment" "UserA" {
+  policy_arn = aws_iam_policy.s3_input_bucket_read_write_policy.arn
+  user       = "UserA"
+  depends_on = [
+    aws_iam_user.UserA
+  ]
+}
+
+resource "aws_iam_user_policy_attachment" "UserB" {
+  policy_arn = aws_iam_policy.s3_processed_bucket_read_only_policy.arn
+  user       = "UserB"
+  depends_on = [
+    aws_iam_user.UserB
+  ]
+}
+
+
+resource "aws_iam_policy" "s3_input_bucket_read_write_policy" {
+  name = "S3-Read-Write-Input-Bucket"
+  path = "/"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetBucketLocation",
+        "s3:ListAllMyBuckets"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": ["s3:ListBucket"],
+      "Resource": ["arn:aws:s3:::image-processor-input"]
+    },
+    {
+      "Action": [
+        "s3:PutObject",
+        "s3:GetObject",
+        "s3:DeleteObject"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "arn:aws:s3:::image-processor-input/*"
+      ]
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_policy" "s3_processed_bucket_read_only_policy" {
+  name = "S3-Read-Only-Processed-Bucket"
+  path = "/"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetBucketLocation",
+        "s3:ListAllMyBuckets"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": ["s3:ListBucket"],
+      "Resource": ["arn:aws:s3:::image-processor-processed"]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Resource": ["arn:aws:s3:::image-processor-processed/*"]
+    }
+  ]
+}
+POLICY
+}
+
