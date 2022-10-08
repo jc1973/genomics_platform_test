35b5cca71ec7d499178b1fd6eb4af6aed576bdef
John Carter <john_p_carter@yahoo.co.uk>
2022-10-08 20:09:23 +0100

    feature: move s3 buckets into lamba

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -18,3 +18,62 @@ module "lambda_function" {
   number_of_policies = 2
 
 }
+
+resource "aws_s3_bucket" "s3_bucket_source" {
+  bucket = "${var.project_name}-source"
+
+  tags = {
+    Name = "${var.project_name} - source"
+  }
+}
+
+resource "aws_s3_bucket_acl" "s3_bucket_source" {
+  bucket = aws_s3_bucket.s3_bucket_source.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket" "s3_bucket_processed" {
+  bucket = "${var.project_name}-processed"
+
+  tags = {
+    Name = "${var.project_name} - processed"
+  }
+}
+
+resource "aws_s3_bucket_acl" "s3_bucket_processed" {
+  bucket = aws_s3_bucket.s3_bucket_processed.id
+  acl    = "private"
+}
+
+
+resource "aws_s3_bucket" "s3_bucket_input" {
+  bucket = "${var.project_name}-input"
+
+  tags = {
+    Name = "${var.project_name} - input"
+  }
+}
+
+resource "aws_s3_bucket_acl" "s3_bucket_input" {
+  bucket = aws_s3_bucket.s3_bucket_input.id
+  acl    = "private"
+}
+
+# Trigger lambda function when a file is put in s3 bucket
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.s3_bucket_input.id
+  lambda_function {
+    lambda_function_arn = module.lambda_function.lambda_function_arn
+    events              = ["s3:ObjectCreated:Put"]
+
+  }
+}
+
+resource "aws_lambda_permission" "input_bucket" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = module.lambda_function.lambda_function_arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::${aws_s3_bucket.s3_bucket_input.id}"
+}
+
