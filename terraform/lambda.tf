acfe0f37f85d07f28d301bcb6086e04b4c6f817e
John Carter <john_p_carter@yahoo.co.uk>
2022-10-08 20:19:41 +0100

    Fix: remove testing bucket

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -19,19 +19,6 @@ module "lambda_function" {
 
 }
 
-resource "aws_s3_bucket" "s3_bucket_source" {
-  bucket = "${var.project_name}-source"
-
-  tags = {
-    Name = "${var.project_name} - source"
-  }
-}
-
-resource "aws_s3_bucket_acl" "s3_bucket_source" {
-  bucket = aws_s3_bucket.s3_bucket_source.id
-  acl    = "private"
-}
-
 resource "aws_s3_bucket" "s3_bucket_processed" {
   bucket = "${var.project_name}-processed"
 
